d761eb716375da104a9697e831b9498e2ab2cad2
Fabio Murtas <you@example.com>
2024-09-09 19:44:25 +0200

    Es4_2

---
+++ src/main/java/org/fabio/murtas/Es4_2.java
@@ -0,0 +1,11 @@
+package org.fabio.murtas;
+
+public class Es4_2 {
+    public static void zool(int num, String name, String street) {
+        System.out.println("Il numero è : " + num + "\nIl nome è : " + name + "\nLa strada è : " + street);
+    }
+
+    public static void main(String[] args) {
+        zool(11, "Giulio", "Via campiglia");
+    }
+}
