3598d6d29445fac13e11c7c35924195f454a8d60
Fabio Murtas <you@example.com>
2024-09-09 13:01:22 +0200

    modifiche app e bug scanner

--- src/main/java/org/fabio/murtas/BugScanner3_1.java
+++ src/main/java/org/fabio/murtas/BugScanner3_1.java
@@ -2,7 +2,7 @@ package org.fabio.murtas;
 
 import java.util.Scanner;
 
-public class BugScanner {
+public class BugScanner3_1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String name = "";
@@ -14,6 +14,15 @@ public class BugScanner {
         System.out.println("Inserisci il tuo nome : ");
         name = in.nextLine();
         System.out.printf("Hello %s, age %d\n", name, age);
+
+        /*
+        //questa può essere una soluzione alternativa
+        System.out.println("Inserisci la tua età : ");
+        age = Integer.parseInt(in.nextLine());
+        System.out.println("Inserisci il tuo nome : ");
+        name = in.nextLine();
+        System.out.printf("Hello %s, age %d\n", name, age);
+        */
 
     }
 }
